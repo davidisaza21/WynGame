d01db59d405466237d6b9389069ccd24acd7312f
Anthony Tamayo <anthonyatamayo@gmail.com>
2015-07-16 15:37:20 -0400

    Added bg image, youtube videos trailers for top 6 games

--- db/schema.rb
+++ db/schema.rb
@@ -13,14 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20150716053100) do
 
-  create_table "authorizations", force: :cascade do |t|
-    t.string   "provider"
-    t.string   "uid"
-    t.integer  "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "products", force: :cascade do |t|
     t.string   "name"
     t.text     "description"
