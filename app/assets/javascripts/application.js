10b80c6c1f363dca026814fd72fa2c1c1244f11b
Weston Cooper <weston@westoncooper.com>
2015-07-17 03:00:05 -0400

    errorsssssssssleeeep

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,19 +9,19 @@
 //
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
-//
-//= require jquery
-//= require jquery_ujs
-//= require turbolinks
-//= require_tree .
-$(document).ready(function() {
-	$('.hoverspan2').on('mouseover', function(e) {
-		var description = $(this).parent().find('.hoverspan');
-		position = $(this).position();
-		description.css({top: position.top + 200, left: position.left + 200});
-		description.show();
-	});
-	$('.hoverspan2').on('mouseout', function(e) {
-		$(this).parent().find('.hoverspan').hide();
-	});
-});
+////
+////= require jquery
+////= require jquery_ujs
+////= require turbolinks
+////= require_tree .
+//$(document).ready(function() {
+//	$('.hoverspan2').on('mouseover', function(e) {
+//		var description = $(this).parent().find('.hoverspan');
+//		position = $(this).position();
+//		description.css({top: position.top + 200, left: position.left + 200});
+//		description.show();
+//	});
+//	$('.hoverspan2').on('mouseout', function(e) {
+//		$(this).parent().find('.hoverspan').hide();
+//	});
+//});
